254c544846e066ac64eeea97672894acc2c860fd
Zoltan Magyar <zolcsi.san@gmail.com>
2024-02-06 15:05:30 +0100

    review comments

--- frontend/src/app/core/service/dark-mode.service.ts
+++ frontend/src/app/core/service/dark-mode.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 /* the service provides a centralized and reusable way to manage dark mode functionality throughout the app. */
 export class DarkModeTogglerService {
   private darkModeSubject: BehaviorSubject<boolean>;
-  public darkMode$: BehaviorSubject<boolean>;
+  public darkMode$: Observable<boolean>;
 
   constructor() {
     this.darkModeSubject = new BehaviorSubject<boolean>(false);
-    this.darkMode$ = this.darkModeSubject;
+    this.darkMode$ = this.darkModeSubject.asObservable();
   }
 
-  toggleDarkMode() {
-    const currentMode = this.darkModeSubject.value;
-    this.darkModeSubject.next(!currentMode);
-  }
-
-  getDarkMode(): boolean {
-    return this.darkModeSubject.value;
+  toggleDarkMode(): void {
+    this.darkModeSubject.next(!this.darkModeSubject.value);
   }
 }
