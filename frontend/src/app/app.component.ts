cd63f304b2a7ed70d51d03bb4cd1ddbfe23ca101
Zoltan Magyar <zolcsi.san@gmail.com>
2024-02-12 09:20:31 +0100

    privacy policy placeholder, user auth (#37)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { UiStateService } from './core/service/ui-state.service';
+import { Pi } from '@pinetwork-js/sdk';
+import { AuthResult } from '@pinetwork-js/sdk/build/types';
+import { SnackService } from './core/service/snack.service';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +14,19 @@ import { UiStateService } from './core/service/ui-state.service';
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  constructor(private readonly uiStateService: UiStateService) {
+export class AppComponent implements OnInit {
+  constructor(
+    private readonly snackService: SnackService,
+    private readonly uiStateService: UiStateService,
+  ) {
     this.uiStateService.setShowBackButton(false);
+  }
+
+  ngOnInit() {
+    Pi.authenticate(['username'], (p) => console.log(p))
+      .then((auth: AuthResult) => this.snackService.showMessage(`Logging in ${auth.user.username}`))
+      .catch(function (error) {
+        console.error(error);
+      });
   }
 }
