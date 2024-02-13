0bdf000b37ebf21d4d93d0db100a6ad72b625349
DarinHajou <it_related@hotmail.com>
2024-02-13 13:58:01 +0100

    Implement JavasScript logic for main content

---
+++ frontend-orig/src/receive-transactions/js/modules/mainContent.js
@@ -0,0 +1,41 @@
+export default function () {
+
+    /**
+     * Initializes event listeners once the DOM is fully loaded.
+     */
+    document.addEventListener('DOMContentLoaded', () => {
+        initializeEventListeners();
+        });
+
+    
+    /**
+     * Sets up event listeners for transaction actions.
+     */
+    function initializeEventListeners() {
+    setupPayButton();
+    setupCancelPurchaseButton();
+    }
+
+    /**
+    * Attaches an event listener to the pay with Pi button.
+    */
+    function setupPayButton() {
+        const payButton = document.querySelector('.actions__button--pay');
+        payButton.addEventListener('click', () => {
+        // Purchase with Pi logic goes here.
+        history.back();
+        console.log('Pay with Pi logic goes here');
+        });
+    }
+
+    /**
+    * Attaches an event listener to the cancel purchase button.
+    */
+    function setupCancelPurchaseButton() {
+        const cancelButton = document.querySelector('.actions__button--cancel');
+        cancelButton.addEventListener('click', () => {
+        // Insert logic to cancel purchase button here.
+        console.log('cancel purchase logic goes here');
+        });
+    }
+}
