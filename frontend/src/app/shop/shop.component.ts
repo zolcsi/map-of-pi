c5b4d4ca4be6bc37c2305cbb019648059b04ba87
swoocn <swooshinn@gmail.com>
2024-02-16 23:45:37 -0500

    Adjustments to formatting; included CSP filter; cleanup Vanilla JS.

--- frontend/src/app/shop/shop.component.ts
+++ frontend/src/app/shop/shop.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 import { ShopInfoComponent } from './shop-info/shop-info.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { LoyaltyInfoComponent } from './loyalty-info/loyalty-info.component';
@@ -8,7 +9,7 @@ import { UiStateService } from '../core/service/ui-state.service';
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [ShoppingCartComponent, ShopInfoComponent, ProductListComponent, LoyaltyInfoComponent],
+  imports: [ShoppingCartComponent, TransactionsComponent, ShopInfoComponent, ProductListComponent, LoyaltyInfoComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- frontend/src/app/shop/vanilla/receive-pi-transactions/js/main.js
+++
@@ -1,5 +0,0 @@
-import walletHeader from "./modules/wallet-header.js";
-import mainContent from "./modules/main-content.js";
-
-walletHeader();
-mainContent();
