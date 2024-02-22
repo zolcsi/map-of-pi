d93588aa8a0a7162a66174e76f1446d3b70a65c9
DarinHajou <it_related@hotmail.com>
2024-02-22 17:48:33 +0100

    Merge branch 'order-and-payment-menu-settings-frontend' of https://github.com/zolcsi/map-of-pi into order-and-payment-menu-settings-frontend

--- frontend/src/app/business/business-settings/vanilla/order-and-payment-menu-settings/js/modules/display-menu-options.js
+++ frontend/src/app/business/business-settings/vanilla/order-and-payment-menu-settings/js/modules/display-menu-options.js
@@ -2,209 +2,153 @@
  * Initializes and handles the toggle functionality for menu settings.
  */
 export default function () {
-    /**
-     * Sets up the initial state and event listeners once the DOM content is fully loaded.
-     */
-    document.addEventListener('DOMContentLoaded', () => {
-        const menuToggle = document.getElementById('menu-toggle');
-        const orderToggleDiv = document.querySelector('.menu-settings__toggle:nth-child(2)');
-        const paymentToggleDiv = document.querySelector('.menu-settings__toggle:nth-child(3)');
-        const addItemButtonSection = document.querySelector('.menu-setting__floating-button-section');
-        const floatingButtonSection = document.querySelector('.menu-setting__floating-button');
-<<<<<<< HEAD
-        loadToggleStates(); // Load the toggle states as the first action
-=======
-        loadToggleStates(); // Load the toggle based on saved states
->>>>>>> 732ea1ee1a2d743df8e3ae553816a0082cdfa415
-
-        /**
-         * Simulates saving the current states of toggle buttons.
-         * Placeholder for backend API call.
-         */
-        function saveToggleStates() {
+   /**
+    * Sets up the initial state and event listeners once the DOM content is fully loaded.
+    */
+   document.addEventListener('DOMContentLoaded', () => {
+      const menuToggle = document.getElementById('menu-toggle');
+      const orderToggleDiv = document.querySelector('.menu-settings__toggle:nth-child(2)');
+      const paymentToggleDiv = document.querySelector('.menu-settings__toggle:nth-child(3)');
+      const addItemButtonSection = document.querySelector('.menu-setting__floating-button-section');
+      const floatingButtonSection = document.querySelector('.menu-setting__floating-button');
+      loadToggleStates(); // Load the toggle states as the first action
+      
+      /**
+       * Simulates saving the current states of toggle buttons.
+       * Placeholder for backend API call.
+       */
+      function saveToggleStates() {
             // Placeholder for saving the current state of toggles to the backend
             console.log('Saving toggle states...');
             // Make API call to the backend
-        }
-        
-<<<<<<< HEAD
-        /**
-=======
-         /**
->>>>>>> 732ea1ee1a2d743df8e3ae553816a0082cdfa415
-         * Loads and applies the saved toggle states from the backend.
-         */
-        function loadToggleStates() {
+      }
+      
+      /**
+       * Loads and applies the saved toggle states from the backend.
+       */
+      function loadToggleStates() {
             // Placeholder for loading saved toggle states from the backend
             console.log('Loading toggle states...');
             // Simulate fetching data from the backend
             // Make an API call to your backend to fetch saved states
-<<<<<<< HEAD
-            // Hardcoded states for demonstration
-            const fetchedStates = {
-                menuEnabled: true,
-                orderEnabled: false,
-=======
             // Hardcoded states for demonstration:
             const fetchedStates = {
-                menuEnabled: false,
-                orderEnabled: true,
->>>>>>> 732ea1ee1a2d743df8e3ae553816a0082cdfa415
-                paymentEnabled: true,
+               menuEnabled: true,
+               orderEnabled: false,
+               paymentEnabled: true,
             };
-        
+      
             // Apply these states to your toggles
             menuToggle.checked = fetchedStates.menuEnabled;
             orderToggleDiv.querySelector('input').checked = fetchedStates.orderEnabled;
             paymentToggleDiv.querySelector('input').checked = fetchedStates.paymentEnabled;
-        
+      
             // Update the UI based on these states
             updateToggleLabels();
             updateSliderAppearance();
             updateFloatingButtonVisibility();
-        }
-        
-        
-        // Initialize the toggles based on the menu setting
-        initializeToggles();     
+      }
+      
+      // Initialize the toggles based on the menu setting
+      initializeToggles();     
 
-        /**
-         * Updates the visibility of the floating add item button based on the menu toggle state.
-         */
-        function updateFloatingButtonVisibility() {
+      /**
+       * Updates the visibility of the floating add item button based on the menu toggle state.
+       */
+      function updateFloatingButtonVisibility() {
             if (menuToggle.checked) {
-                floatingButtonSection.classList.remove('hidden');
+               floatingButtonSection.classList.remove('hidden');
             } else {
-                floatingButtonSection.classList.add('hidden');
+               floatingButtonSection.classList.add('hidden');
             }
-        }
-<<<<<<< HEAD
-     /**
-      * Updates the labels next to toggles to reflect their current state off/on.
-      */    
-    function updateToggleLabels() {
-        const menuStatusLabel = document.querySelector('label[for="menu-toggle"] .toggle-status');
-        const orderStatusLabel = document.querySelector('label[for="order-toggle"] .toggle-status');
-        const paymentStatusLabel = document.querySelector('label[for="payment-toggle"] .toggle-status');
-    
-        menuStatusLabel.textContent = menuToggle.checked ? 'on' : 'off';
-        orderStatusLabel.textContent = orderToggleDiv.querySelector('input').checked ? 'on' : 'off';
-        paymentStatusLabel.textContent = paymentToggleDiv.querySelector('input').checked ? 'on' : 'off';
-    }
-=======
+      }
 
-          /**
-         * Updates the labels next to toggles to reflect their current state off/on.
-         */
-         function updateToggleLabels() {
-            const menuStatusLabel = document.querySelector('label[for="menu-toggle"] .toggle-status');
-            const orderStatusLabel = document.querySelector('label[for="order-toggle"] .toggle-status');
-            const paymentStatusLabel = document.querySelector('label[for="payment-toggle"] .toggle-status');
-         
-            menuStatusLabel.textContent = menuToggle.checked ? 'on' : 'off';
-            orderStatusLabel.textContent = orderToggleDiv.querySelector('input').checked ? 'on' : 'off';
-            paymentStatusLabel.textContent = paymentToggleDiv.querySelector('input').checked ? 'on' : 'off';
-         }
->>>>>>> 732ea1ee1a2d743df8e3ae553816a0082cdfa415
+      
+      /**
+       * Updates the labels next to toggles to reflect their current state off/on.
+       */
+      function updateToggleLabels() {
+         const menuStatusLabel = document.querySelector('label[for="menu-toggle"] .toggle-status');
+         const orderStatusLabel = document.querySelector('label[for="order-toggle"] .toggle-status');
+         const paymentStatusLabel = document.querySelector('label[for="payment-toggle"] .toggle-status');
+      
+         menuStatusLabel.textContent = menuToggle.checked ? 'on' : 'off';
+         orderStatusLabel.textContent = orderToggleDiv.querySelector('input').checked ? 'on' : 'off';
+         paymentStatusLabel.textContent = paymentToggleDiv.querySelector('input').checked ? 'on' : 'off';
+      }
 
-        // Initialize labels based on the current state
-        updateToggleLabels();
-        
-<<<<<<< HEAD
-        // Update labels on toggle change
-=======
-        // Set label text based on toggle state
->>>>>>> 732ea1ee1a2d743df8e3ae553816a0082cdfa415
-        document.querySelectorAll('.menu-settings__toggle input').forEach(toggle => {
+      // Initialize labels based on the current state
+      updateToggleLabels();
+      
+      // Update labels on toggle change
+      document.querySelectorAll('.menu-settings__toggle input').forEach(toggle => {
             toggle.addEventListener('change', updateToggleLabels);
-        });
+      });
 
-        // Initialize floating button visibility based on the current state of the menu toggle
-        updateFloatingButtonVisibility();
+      // Initialize floating button visibility based on the current state of the menu toggle
+      updateFloatingButtonVisibility();
 
-        // Add event listener to menu toggle to update floating button visibility on change
-        menuToggle.addEventListener('change', updateFloatingButtonVisibility);
+      // Add event listener to menu toggle to update floating button visibility on change
+      menuToggle.addEventListener('change', updateFloatingButtonVisibility);
 
-        // Update appearance on changes
-        document.querySelectorAll('.menu-settings__toggle input').forEach(toggle => {
+      // Update appearance on changes
+      document.querySelectorAll('.menu-settings__toggle input').forEach(toggle => {
             toggle.addEventListener('change', function() {
-                updateSliderAppearance();
-                // Additional logic based on specific toggle
-                if (toggle.id === 'menu-toggle') {
-                    if (toggle.checked) {
+               updateSliderAppearance();
+               // Additional logic based on specific toggle
+               if (toggle.id === 'menu-toggle') {
+                  if (toggle.checked) {
                         orderToggleDiv.classList.remove('hidden');
                         paymentToggleDiv.classList.remove('hidden');
                         addItemButtonSection.classList.remove('hidden');
-                    } else {
+                  } else {
                         orderToggleDiv.classList.add('hidden');
                         paymentToggleDiv.classList.add('hidden');
                         addItemButtonSection.classList.add('hidden');
-                    }
-                }
+                  }
+               }
             });
-        });
+      });
 
-<<<<<<< HEAD
-=======
-        // Setup event listeners for toggles
->>>>>>> 732ea1ee1a2d743df8e3ae553816a0082cdfa415
-        document.querySelectorAll('.menu-settings__toggle input').forEach(toggle => {
+      document.querySelectorAll('.menu-settings__toggle input').forEach(toggle => {
             toggle.addEventListener('change', function() {
-                updateSliderAppearance();
-                updateToggleLabels();
-                saveToggleStates(); // Save the new states after any toggle changes
+               updateSliderAppearance();
+               updateToggleLabels();
+               saveToggleStates(); // Save the new states after any toggle changes
             });
-        });
-<<<<<<< HEAD
+      });
 
-=======
-        
->>>>>>> 732ea1ee1a2d743df8e3ae553816a0082cdfa415
-        function initializeToggles() {
+      function initializeToggles() {
             // Initial state update
             updateSliderAppearance();
             if (menuToggle.checked) {
-                orderToggleDiv.classList.remove('hidden');
-                paymentToggleDiv.classList.remove('hidden');
-                addItemButtonSection.classList.remove('hidden');
+               orderToggleDiv.classList.remove('hidden');
+               paymentToggleDiv.classList.remove('hidden');
+               addItemButtonSection.classList.remove('hidden');
             } else {
-                orderToggleDiv.classList.add('hidden');
-                paymentToggleDiv.classList.add('hidden');
-                addItemButtonSection.classList.add('hidden');
+               orderToggleDiv.classList.add('hidden');
+               paymentToggleDiv.classList.add('hidden');
+               addItemButtonSection.classList.add('hidden');
             }
-        }
-    });
+      }
+      });
 
-    /**
-<<<<<<< HEAD
-     * Updates the appearance of sliders to reflect their state.
-     */
-=======
+   /**
    * Updates the appearance of sliders to reflect their state.
    */
->>>>>>> 732ea1ee1a2d743df8e3ae553816a0082cdfa415
-    function updateSliderAppearance() {
-        const toggles = document.querySelectorAll('.menu-settings__toggle input');
-    
-        toggles.forEach(toggle => {
-            const slider = toggle.nextElementSibling; // Select the slider span related to each toggle
-            const sliderCircle = slider.querySelector('.menu-settings__slider:before');
-<<<<<<< HEAD
-            if (toggle.checked) {
-                slider.classList.remove('menu-settings__slider--off');
-=======
+   function updateSliderAppearance() {
+      const toggles = document.querySelectorAll('.menu-settings__toggle input');
+   
+      toggles.forEach(toggle => {
+            const slider = toggle.nextElementSibling; // Accurately select the slider span related to each toggle
+            const sliderCircle = slider.querySelector('.menu-settings__slider:before'); // Assuming you want to target the pseudo-element (note: pseudo-elements cannot be directly manipulated via JS like this)
 
             if (toggle.checked) {
-             
->>>>>>> 732ea1ee1a2d743df8e3ae553816a0082cdfa415
+               slider.classList.remove('menu-settings__slider--off');
             } else {
-                slider.classList.add('menu-settings__slider--off');
+               slider.classList.add('menu-settings__slider--off');
+               // If you have a specific class to indicate the "off" state that affects the appearance, toggle it here
             }
-        });
-    }
+      });
+   }
 }
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 732ea1ee1a2d743df8e3ae553816a0082cdfa415
