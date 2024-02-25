ad030fd8f025b3ad4c154549aa4a13aaba12019c
swoocn <swooshinn@gmail.com>
2024-02-25 08:46:45 -0500

    Attempt to fix broken build.

--- frontend/src/app/business/business.routes.ts
+++ frontend/src/app/business/business.routes.ts
@@ -15,7 +15,7 @@ export const BUSINESS_ROUTES: Routes = [
     loadComponent: () => import('./loyalty-scan/loyalty-scan.component').then((m) => m.LoyaltyScanComponent),
   },
   {
-    path: 'business-configuration',
+    path: 'business-config',
     loadComponent: () => import('./business-settings/business-configuration/business-configuration.component').then((m) => m.BusinessConfigurationComponent),
   },
   {
