90727c31f92306681f148beeec19ee77a77f3014
swoocn <swooshinn@gmail.com>
2024-02-27 22:24:52 -0500

    Business settings revision; WIP.

--- frontend/src/app/business/business-settings/business-settings.component.ts
+++ frontend/src/app/business/business-settings/business-settings.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { SnackService } from '../../core/service/snack.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ShopService } from '../../core/service/shop.service';
 
 @Component({
@@ -12,12 +12,11 @@ import { ShopService } from '../../core/service/shop.service';
   standalone: true,
   templateUrl: './business-settings.component.html',
   styleUrls: ['./business-settings.component.scss'],
-  imports: [TranslateModule, CommonModule, ReactiveFormsModule, MatSlideToggleModule],
+  imports: [TranslateModule, CommonModule, ReactiveFormsModule, MatSlideToggleModule, RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BusinessSettingsComponent {
-  businessTypes: string[] = ['Restaurant', 'Retail', 'Service', 'construction', 'driving'];
-  mustAllowtransactions: boolean = true;
+  businessTypes: string[] = ['Restaurant', 'Retail Store', 'Servicing', 'Construction', 'Transportation'];
   router: Router = inject(Router);
 
   registerShopForm = new FormGroup({
@@ -26,16 +25,14 @@ export class BusinessSettingsComponent {
     shopAddress: new FormControl('', Validators.required),
     shopPhone: new FormControl('', [Validators.required, Validators.minLength(10)]),
     shopEmail: new FormControl('', [Validators.required, Validators.email]),
-    isPiPaymentEnabled: new FormControl(true),
     shopImage: new FormControl('', Validators.required),
     shopDescription: new FormControl('', Validators.required),
   });
 
   constructor(
-    private readonly formBuilder: FormBuilder,
     private snackService: SnackService,
     private shopServices: ShopService,
-  ) {}
+  ) { }
 
   onFileChange(event: any) {
     if (event.target.files) {
@@ -52,18 +49,17 @@ export class BusinessSettingsComponent {
       this.shopServices.registerShop(this.registerShopForm.value as any).then((response) => {
         // const { newShop, currentUser } = response.data;
         if (response.ok) {
-          this.snackService.showError('Succesfuly added shop');
+          this.snackService.showMessage('Business successfully registered');
         } else {
-          this.snackService.showError('erro while registering shop');
+          this.snackService.showError('Failed to register business');
           console.log(response);
         }
       });
 
-      this.registerShopForm.value.isPiPaymentEnabled = true;
       this.router.navigate(['business', 'manage-business']);
     } else {
       this.registerShopForm.markAllAsTouched();
-      console.log('ivalid data');
+      console.log('Invalid data');
     }
   }
 }
