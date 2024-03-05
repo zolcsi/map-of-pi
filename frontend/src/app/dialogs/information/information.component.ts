b74dd15501906f897226f2e4247da863e19ac9e9
swoocn <swooshinn@gmail.com>
2024-03-05 08:41:55 -0500

    Generate new Angular components for Information dialog.

---
+++ frontend/src/app/dialogs/information/information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './information.component.html',
+  styleUrl: './information.component.scss'
+})
+export class InformationComponent {
+
+}
