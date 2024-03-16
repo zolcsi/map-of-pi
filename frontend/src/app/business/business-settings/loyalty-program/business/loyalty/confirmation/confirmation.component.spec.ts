3d922115b9cbd18ff0ffd59b4995fadd4f7a1980
DarinHajou <it_related@hotmail.com>
2024-03-16 16:57:52 +0100

    Create loyalty confirmation component

---
+++ frontend/src/app/business/business-settings/loyalty-program/business/loyalty/confirmation/confirmation.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConfirmationComponent } from './confirmation.component';
+
+describe('ConfirmationComponent', () => {
+  let component: ConfirmationComponent;
+  let fixture: ComponentFixture<ConfirmationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ConfirmationComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ConfirmationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/business/business-settings/loyalty-program/business/loyalty/confirmation/confirmation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirmation.component.html',
+  styleUrl: './confirmation.component.scss'
+})
+export class ConfirmationComponent {
+
+}
