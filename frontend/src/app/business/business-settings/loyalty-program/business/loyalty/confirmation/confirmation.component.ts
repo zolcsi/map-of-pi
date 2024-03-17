77c5103e3d895292fef9ad08ab2794278f3a9b16
DarinHajou <it_related@hotmail.com>
2024-03-17 18:10:49 +0100

    Setup Initial State Handling in ConfirmationComponent

--- frontend/src/app/business/business-settings/loyalty-program/business/loyalty/confirmation/confirmation.component.ts
+++ frontend/src/app/business/business-settings/loyalty-program/business/loyalty/confirmation/confirmation.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirmation',
   standalone: true,
-  imports: [],
   templateUrl: './confirmation.component.html',
-  styleUrl: './confirmation.component.scss'
+  styleUrls: ['./confirmation.component.scss']
 })
-export class ConfirmationComponent {
+export class ConfirmationComponent implements OnInit {
+  stampsNeeded?: number;
+  itemName?: string;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router // Inject the Router service
+  ) {}
+
+  ngOnInit(): void {
+    // Retrieve the current navigation from the Router service
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation?.extras.state as { stampsNeeded: number; itemName: string };
+
+    if (state) {
+      this.stampsNeeded = state.stampsNeeded;
+      this.itemName = state.itemName;
+    }
+  }
+
+  onEdit() {
+    // Implement the edit logic or navigation
+  }
+
+  onConfirm() {
+    // Implement the confirm logic or navigation
+  }
 }
+
